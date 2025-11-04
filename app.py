0ef854f441929b5b3ba890e814a4e9a9c5b17cfc
Victor Ricardo Oliveira Nunes <victor12092004@gmail.com>
2025-11-04 18:01:26 -0300

    [feat] pequenos ajustes

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/qrcode')
 def qrcode():
     return render_template('qrcode.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
